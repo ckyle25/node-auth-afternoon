c764ebfb3a241faeb95f388110f8621915dc76be
Kyle Cornwall <kyle.cornwall@healthcatalyst.com>
2018-02-15 09:47:48 -0700

    cc commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport')
+const strategy = require('./strategy')
+const request = require('request')
+const axios = require('axios')
 
 const app = express();
 app.use( session({
@@ -7,6 +11,44 @@ app.use( session({
   resave: false,
   saveUninitialized: false
 }));
+app.use( passport.initialize() );
+app.use( passport.session() );
+passport.use( strategy );
+
+passport.serializeUser(function(user,done){
+  const { _json } = user;
+  done(null, { clientID: _json.clientID, 
+               email: _json.email, 
+               name: _json.name, 
+               followers: _json.followers_url });
+})
+
+passport.deserializeUser(function(obj,done){
+  done(null, obj)
+})
+
+app.get('/login',
+  passport.authenticate('auth0',{
+    successRedirect:'/followers',
+    failureRedirect:'/login',
+    failureFlash:true,
+    connection:'github'
+  })
+)
+
+app.get('/followers',function(req,res,next){
+  console.log(req.session.user)
+  if(!req.user){
+    res.redirect('/login')
+  } else {
+    let baseURL = 'https://api.github.com/'
+    axios.get(`${baseURL}users/ckyle25/followers`)
+      .then(response => {
+        return response
+      })
+  }
+}
+)
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
